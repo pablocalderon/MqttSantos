052d5deebaa04e2c88e8455115e3adafc164ec97
Pablo Calderon <pablocalderoncornejo@gmail.com>
2022-11-28 16:41:10 -0300

    Initial commit

---
+++ app/src/main/java/cl/isisur/mqttsantos/MainActivity.java
@@ -0,0 +1,81 @@
+package cl.isisur.mqttsantos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MqttAndroidClient client;
+    private final MemoryPersistence persistence = new MemoryPersistence();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    //++++++++++++++++++++++++++++++++++++++++++++
+        final MqttAndroidClient mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), "mqtt://test.mosquitto.org:1883", "SensorSantos", persistence);
+            mqttAndroidClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                System.out.println("Connection was lost!");
+               }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Message Arrived!: " + topic + ": " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("Delivery Complete!");
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setCleanSession(true);
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Connection Success!");
+                    try {
+                        System.out.println("Subscribing to /test");
+                        mqttAndroidClient.subscribe("/test", 0);
+                        System.out.println("Subscribed to /test");
+                        System.out.println("Publishing message..");
+                        mqttAndroidClient.publish("/test", new MqttMessage("Hola santo tomas Chillán..!".getBytes()));
+                    } catch (MqttException ex) {
+
+                    }
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Connection Failure!");
+                    System.out.println("throwable: " + exception.toString());
+                }
+            });
+        } catch (MqttException ex) {
+            System.out.println(ex.toString());
+        }
+
+
+        //++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
